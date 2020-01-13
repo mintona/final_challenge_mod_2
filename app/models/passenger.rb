cdd1d8fde808ddffa2809bd41f8a1299e4a4438b
Alison Vermeil <alison.minton@gmail.com>
2020-01-13 09:36:28 -0700

    Add relationships after creating flight_passenger joins table

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,7 @@
 class Passenger < ApplicationRecord
+  validates_presence_of :name, :age
+  validates_numericality_of :age, greater_than: 0
 
+  has_many :passenger_flights
+  has_many :flights, through: :passenger_flights
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -8,7 +8,10 @@ RSpec.describe Flight, type: :model do
     it {should validate_presence_of :departure_city}
     it {should validate_presence_of :arrival_city}
   end
+
   describe 'relationships' do
     it {should belong_to :airline}
+    it {should have_many :passenger_flights}
+    it {should have_many(:passengers).through(:passenger_flights)}
   end
 end
