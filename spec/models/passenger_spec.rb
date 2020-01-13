e2cfe85aca044edb76699409c30f4f64f9057ef3
Alison Vermeil <alison.minton@gmail.com>
2020-01-13 09:35:54 -0700

    Add validation for age

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Passenger, type: :model do
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
+    it {should validate_numericality_of(:age).is_greater_than(0)}
   end
   describe 'relationships' do
     it {should have_many :passenger_flights}
