0e2a44d64f0c7090c6f9dd2eb1795f8f145c8561
Alison Vermeil <alison.minton@gmail.com>
2020-01-13 10:46:53 -0700

    Add update action to passenger controller. Story 3 complete

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,12 @@ class PassengersController < ApplicationController
   def show
     @passenger = Passenger.find(params[:id])
   end
+
+  def update
+    passenger = Passenger.find(params[:id])
+    flight = Flight.find_by(number: params[:flight_number])
+    PassengerFlight.create(passenger: passenger, flight: flight)
+
+    redirect_to passenger_path(params[:id])
+  end
 end
