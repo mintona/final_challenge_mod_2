faa1ef164781d29b6fd1f247b0d706ccdeefece3
Alison Vermeil <alison.minton@gmail.com>
2020-01-13 11:25:35 -0700

    Remove update from passengers and refactor into passenger flights controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,12 +2,4 @@ class PassengersController < ApplicationController
   def show
     @passenger = Passenger.find(params[:id])
   end
-
-  def update
-    passenger = Passenger.find(params[:id])
-    flight = Flight.find_by(number: params[:flight_number])
-    flight.passengers << passenger
-
-    redirect_to passenger_path(params[:id])
-  end
 end
